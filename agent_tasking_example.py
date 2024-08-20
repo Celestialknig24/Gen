1ead806c499cb3995e6991a6b4e37307d2f06fc6
Celestialknig24 <66995275+Celestialknig24@users.noreply.github.com>
2024-08-20 16:40:16 +0530

    Update agent_tasking_example.py

--- agent_tasking_example.py
+++ agent_tasking_example.py
@@ -25,3 +25,42 @@ if task_output.json_dict:
     print(f"JSON Output: {json.dumps(task_output.json_dict, indent=2)}")
 if task_output.pydantic:
     print(f"Pydantic Output: {task_output.pydantic}")
+
+
+
+# efsegdf
+import os
+os.environ["OPENAI_API_KEY"] = "Your Key"
+os.environ["SERPER_API_KEY"] = "Your Key" # serper.dev API key
+
+from crewai import Agent, Task, Crew
+from crewai_tools import SerperDevTool
+
+research_agent = Agent(
+  role='Researcher',
+  goal='Find and summarize the latest AI news',
+  backstory="""You're a researcher at a large company.
+  You're responsible for analyzing data and providing insights
+  to the business.""",
+  verbose=True
+)
+
+search_tool = SerperDevTool()
+
+task = Task(
+  description='Find and summarize the latest AI news',
+  expected_output='A bullet list summary of the top 5 most important AI news',
+  agent=research_agent,
+  tools=[search_tool]
+)
+
+crew = Crew(
+    agents=[research_agent],
+    tasks=[task],
+    verbose=True
+)
+
+result = crew.kickoff()
+print(result)
+
+
